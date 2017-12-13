b74f82924393a84226fab40d5694a6b73ec61a44
丰硕 <fengshuochn@126.com>
2017-12-14 00:37:03 +0800

    perf: change to apollo

--- src/main.js
+++ src/main.js
@@ -3,13 +3,27 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import { ApolloClient, createBatchingNetworkInterface } from 'apollo-client'
+import VueApollo from 'vue-apollo'
 
+// Create the apollo client
+const apolloClient = new ApolloClient({
+  networkInterface: createBatchingNetworkInterface({
+    uri: 'http://localhost:3020/graphql'
+  }),
+  connectToDevTools: true
+})
+const apolloProvider = new VueApollo({
+  defaultClient: apolloClient
+})
+
+Vue.use(VueApollo)
 Vue.config.productionTip = false
-
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  apolloProvider,
   template: '<App/>',
   components: { App }
 })
