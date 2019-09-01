9bf685effcb739dffb8e1a3c342c0a58ac3a112d
丰硕 <fengshuochn@126.com>
2019-09-01 21:50:06 +0800

    feat: new parser

---
+++ precache-manifest.84de62666001208047d883ca6bfd43db.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "507602f196992a4496b36579a268747b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dd1761286b8df37a9dfd",
+    "url": "/static/css/2.1c7ffa40.chunk.css"
+  },
+  {
+    "revision": "e496740bad36b8cda130",
+    "url": "/static/css/main.2a991a3a.chunk.css"
+  },
+  {
+    "revision": "dd1761286b8df37a9dfd",
+    "url": "/static/js/2.acd01f7b.chunk.js"
+  },
+  {
+    "revision": "e496740bad36b8cda130",
+    "url": "/static/js/main.bea09d89.chunk.js"
+  },
+  {
+    "revision": "78da098aacbcbe7f403b",
+    "url": "/static/js/runtime~main.6d3e868c.js"
+  },
+  {
+    "revision": "ab11eb2ca3d1c0997ee77334f47e7c12",
+    "url": "/static/media/edelsans_regular.ab11eb2c.otf"
+  }
+]);
--- src/config.js
+++
@@ -1,3 +0,0 @@
-export default {
-  readOnlyToken: '6#c9fa384dd4a6bc6b367df1a6d83d2ad9479ca37'
-}
